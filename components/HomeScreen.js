a11f9dd31e3d55fac8e0b1c55a5a991a5cca0169
mjcunninghm <89094719+mjcunninghm@users.noreply.github.com>
2023-02-12 15:53:01 -0600

    Created dropdown picker

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -1,21 +1,7 @@
 import * as React from 'react';
-import {
-  View,
-  Button,
-  Text,
-  SafeAreaView,
-  StyleSheet,
-  Dimensions,
-  ImageBackground,
-  Image,
-  FlatList,
-  StatusBar,
-  TouchableOpacity,
-  TextInput,
-  ScrollView,
-  RefreshControl,
-} from 'react-native';
-import {ScreenBar} from './DropdownPicker';
+import {View, Text, SafeAreaView, StyleSheet, Dimensions} from 'react-native';
+import {Picker} from '@react-native-picker/picker';
+import { useEffect } from "react";
 
 const Dev_Height = Dimensions.get('screen').height;
 const Dev_Width = Dimensions.get('screen').width;
@@ -23,17 +9,28 @@ const Dev_Width = Dimensions.get('screen').width;
 export default class HomeScreen extends React.Component {
   constructor(props) {
     super(props);
-    this.pageName = 'Home Screen';
+    this.state = {
+      selectedValue: 'Home',
+    };
   }
-
+  componentDidMount() {
+    this.setState({selectedValue: 'Home'});
+  }
   render() {
     return (
       <SafeAreaView styles={styles.container}>
         <View>
-          <Button
-            title="Go to patient portal"
-            onPress={() => this.props.navigation.navigate('PatientPortal')}
-          />
+          <Picker
+            selectedValue={this.state.selectedValue}
+            style={{height: 30, width: Dev_Width}}
+            onValueChange={(itemValue, itemIndex) =>
+              this.setState({selectedValue: itemValue})
+            }>
+            <Picker.Item label="Home" value={this.props.navigation.navigate('HomeScreen')}/>
+            <Picker.Item label="Patient Portal" value={this.props.navigation.navigate('PatientPortal')} />
+            <Picker.Item label="Care Team Portal" value="care team portal" />
+          </Picker>
+          <Text>{this.state.selectedValue}</Text>
         </View>
       </SafeAreaView>
     );
