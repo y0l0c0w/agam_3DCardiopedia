ab377afe408c7540fb170fa7b3eebf0b819d49e8
mjcunninghm <89094719+mjcunninghm@users.noreply.github.com>
2023-02-14 03:52:45 -0600

    Initial commit

---
+++ components/CareTeamPortal.js
@@ -0,0 +1,48 @@
+import * as React from 'react';
+import {
+  View,
+  Button,
+  Text,
+  SafeAreaView,
+  StyleSheet,
+  Dimensions,
+  ImageBackground,
+  Image,
+  FlatList,
+  StatusBar,
+  TouchableOpacity,
+  TextInput,
+  ScrollView,
+  RefreshControl,
+} from 'react-native';
+
+const Dev_Height = Dimensions.get('screen').height;
+const Dev_Width = Dimensions.get('screen').width;
+
+export default class CareTeamPortal extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <SafeAreaView styles={styles.container}>
+        <View>
+          <Button
+            title="Go to home screen"
+            onPress={() => this.props.navigation.navigate('HomeScreen')}
+          />
+        </View>
+      </SafeAreaView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    height: Dev_Height,
+    width: Dev_Width,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#222222',
+  },
+});
